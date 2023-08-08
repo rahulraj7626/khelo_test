8edd173c9c2613d2194d5a8fe65629887d4aa326
rahulraj7626 <rahulraj7626@gmail.com>
2023-08-09 00:38:54 +0530

    permission added

--- lib/src/home/data/datasources/game_image_list.dart
+++ lib/src/home/data/datasources/game_image_list.dart
@@ -3,10 +3,10 @@ import '../../../config/constants/color_constants.dart';
 List<String> gameImageList = [
   "${imagePath}game1.png",
   "${imagePath}game2.webp",
-  "${imagePath}game3.webp",
-  "${imagePath}game4.webp",
   "${imagePath}game1.png",
   "${imagePath}game2.webp",
-  "${imagePath}game3.webp",
+  "${imagePath}game1.png",
+  "${imagePath}game1.png",
+  "${imagePath}game2.webp",
   "${imagePath}game4.webp"
 ];
